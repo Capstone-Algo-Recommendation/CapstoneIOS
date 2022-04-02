0e6403ec9024064ea70d6071d044a645526f2ea8
yleer <spsyoudong1@naver.com>
2022-04-02 17:31:35 +0900

    SpecificProblem VC에서 문제보기 버튼 누르면 백준 싸이트로 이동 완료

--- CapstonePractice/SpecificProblem View/SpecificProblemViewController.swift
+++ CapstonePractice/SpecificProblem View/SpecificProblemViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class SpecificProblemViewController: UIViewController {
     
     var problemTitle: String?
     let mainView = SpecificProblemView()
     var problemInfo: Item?
+    let disposeBag = DisposeBag()
     
     override func loadView() {
         self.view = mainView
@@ -21,6 +24,14 @@ class SpecificProblemViewController: UIViewController {
         super.viewDidLoad()
         title = problemTitle
         mainView.problemInfoLabel.text = problemInfo?.tags[0].key
+        
+        mainView.problemLinkButton
+            .rx.tap
+            .bind {
+                let vc = BackJoonProblemViewController()
+                vc.questionNumber = self.problemInfo?.problemID
+                self.navigationController?.pushViewController(vc, animated: true)
+            }.disposed(by: disposeBag)
     }
     
 }
