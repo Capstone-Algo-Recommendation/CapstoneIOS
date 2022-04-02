34d09153558f1898b1c0c7351193ad7d0065bcdf
yleer <spsyoudong1@naver.com>
2022-04-02 17:38:09 +0900

    SpecificProblemVC textview placeholder done.

--- CapstonePractice/SpecificProblem View/SpecificProblemViewController.swift
+++ CapstonePractice/SpecificProblem View/SpecificProblemViewController.swift
@@ -32,6 +32,43 @@ class SpecificProblemViewController: UIViewController {
                 vc.questionNumber = self.problemInfo?.problemID
                 self.navigationController?.pushViewController(vc, animated: true)
             }.disposed(by: disposeBag)
+        
+        
+        mainView.memoTextView
+            .rx.didBeginEditing
+            .bind {
+                if self.mainView.memoTextView.textColor == UIColor.gray {
+                    self.mainView.memoTextView.text = nil
+                    self.mainView.memoTextView.textColor = UIColor.black
+                   }
+            }.disposed(by: disposeBag)
+        
+        mainView.fixTextView
+            .rx.didBeginEditing
+            .bind {
+                if self.mainView.fixTextView.textColor == UIColor.gray {
+                    self.mainView.fixTextView.text = nil
+                    self.mainView.fixTextView.textColor = UIColor.black
+                   }
+            }.disposed(by: disposeBag)
+        
+        mainView.memoTextView
+            .rx.didEndEditing
+            .bind {
+                if self.mainView.memoTextView.text.isEmpty {
+                    self.mainView.memoTextView.text = "메모를 입력해주세요."
+                    self.mainView.memoTextView.textColor = UIColor.gray
+                }
+            }.disposed(by: disposeBag)
+        
+        mainView.fixTextView
+            .rx.didEndEditing
+            .bind {
+                if self.mainView.fixTextView.text.isEmpty {
+                    self.mainView.fixTextView.text = "오답 노트를 입력해주세요."
+                    self.mainView.fixTextView.textColor = UIColor.gray
+                }
+            }.disposed(by: disposeBag)
     }
     
 }
